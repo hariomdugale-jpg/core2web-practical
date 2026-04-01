34b21ddb1bcae429b80ed1cd2e89c6d12cecd2d0
hariom ugale <hariomdugale@gmail.com>
2026-04-01 23:18:26 +0530

    Add practical sheet 1 programs

---
+++ practical sheet 1/code10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void main(){
+	int num =15;
+	printf("num = 15 \n");
+	if(num/3==0){
+		printf("Number is Divisible by 3\n");
+
+	}else{
+		printf("Number is not divisible by   3\n");
+
+	}
+	num =17;
+	printf("num = 17 \n");
+	if(num/3==0){
+		printf("Number is Divisible by 3\n");
+
+	}else{
+		printf("Number is not divisible by 3\n");
+
+	}
+}
---
+++ practical sheet 1/code6.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main (){
+	int num =10;
+	if(num>5){
+		printf("number is greater than 5");
+
+	}else{
+		printf("number is less than 5 ");
+
+	}
+
+
+}
